73f4afb642f4e52c4949a2c117de3a2fe0ba0590
Calvin Wang <calvin.wang0628@gmail.com>
2020-02-11 23:51:46 -0800

    Changed nav bar

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -13,17 +13,16 @@ const Nav = () => (
         <img src={`${BASE_PATH}/images/me_icon.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Calvin Wang</h2>
+        <p><a href="mailto:calvin.wang0628@gmail.com">calvin.wang0628@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-            I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-            the co-founder and CTO of <a href="http://arthena.com">Arthena</a>. Before Arthena I was
-            at <a href="http://matroid.com">Matroid</a>, <a href="http://planet.com">Planet</a>, <a href="http://planetaryresources.com">Planetary Resources</a>, <a href="http://facebook.com">Facebook</a>, and <a href="http://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Calvin. I like building things.
+            I am currently a Data Scientist at <a href="https://aws.amazon.com/">AWS</a> and previously a graduate from 
+            <a href="https://ccs.ucsb.edu/"> UC Santa Barbara's  College of Creative Studies</a>.
       </p>
       <ul className="actions">
         <li>
